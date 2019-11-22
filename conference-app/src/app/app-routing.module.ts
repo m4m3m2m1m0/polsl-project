4d2e2765d2811a94c2092287f4869e38ceece0cf
Milosz Zieba <zieba.milosz.k@gmail.com>
2019-11-22 23:16:23 +0100

    sign-inup - add routing

--- conference-app/src/app/app-routing.module.ts
+++ conference-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './cms-components/sign-in-up/login/login.component';
+import { RegisterComponent } from './cms-components/sign-in-up/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
