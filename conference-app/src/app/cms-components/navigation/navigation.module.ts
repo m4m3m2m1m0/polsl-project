b275a0d86ce53cd7c045291e51be97dc11a0c887
Milosz Zieba <zieba.milosz.k@gmail.com>
2019-11-23 13:40:23 +0100

    sign-inup - add user actions buttons to navigation

--- conference-app/src/app/cms-components/navigation/navigation.module.ts
+++ conference-app/src/app/cms-components/navigation/navigation.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavMenuComponent, SideMenuComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [NavMenuComponent, SideMenuComponent],
   entryComponents: [NavMenuComponent, SideMenuComponent]
