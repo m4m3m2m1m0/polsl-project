6f2cf06b883256613cfe54ab0d8c25e89a4fb583
Milosz Zieba <zieba.milosz.k@gmail.com>
2020-01-01 21:49:41 +0100

    open-maps - edit popup with conference details

--- conference-app/src/app/cms-components/open-maps/conference-dialog/conference-dialog.component.ts
+++ conference-app/src/app/cms-components/open-maps/conference-dialog/conference-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-conference-dialog',
@@ -12,15 +13,20 @@ export class ConferenceDialogComponent {
   conference: any;
 
   constructor(
-    private dialogRef: MatDialogRef<ConferenceDialogComponent>,
+    protected _router: Router,
+    protected _dialogRef: MatDialogRef<ConferenceDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data) {
-
     this.description = data.description;
     this.conference = data.conference;
   }
 
   close() {
-    this.dialogRef.close();
+    this._dialogRef.close();
+  }
+
+  showConferenceDetails(confId) {
+    this._router.navigateByUrl(`/conference/${confId}`);
+    this._dialogRef.close();
   }
 
 }
